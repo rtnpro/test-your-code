61cf3e6a601f1f7babe744426814c6a06f53fe7a
rtnpro <rtnpro@gmail.com>
2013-08-28 17:56:51 +0530

    Added code and tests for factorial example.

---
+++ test_your_code/examples/factorial/factorial.py
@@ -0,0 +1,36 @@
+import sys
+
+
+def fact(n):
+    """
+    Factorial function
+
+    :arg n: Number
+    :returns: factorial of n
+
+    >>> fact(0)
+    1
+    >>> fact(4)
+    24
+    """
+    if n == 0:
+        return 1
+    return n * fact(n -1)
+
+
+def div(n):
+    """
+    Just divide
+    """
+    res = 10 / n
+    return res
+
+
+def main(n):
+    res = fact(n)
+    print res
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(int(sys.argv[1]))
---
+++ test_your_code/examples/factorial/test_factorial.py
@@ -0,0 +1,19 @@
+import unittest
+from factorial import fact
+
+class TestFactorial(unittest.TestCase):
+    """
+    Our basic test class
+    """
+
+    def test_fact(self):
+        """
+        The actual test.
+        Any method which starts with ``test_`` will considered as a test case.
+        """
+        res = fact(5)
+        self.assertEqual(res, 120)
+
+
+if __name__ == '__main__':
+    unittest.main() 
