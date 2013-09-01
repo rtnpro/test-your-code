48681d6022dd528f6770457c06bf12996cb2542a
rtnpro <rtnpro@gmail.com>
2013-09-01 14:56:39 +0530

    Improve my_repos example and added tests for it.

--- test_your_code/examples/my_repos/myrepos.py
+++ test_your_code/examples/my_repos/myrepos.py
@@ -4,12 +4,16 @@ import json
 
 URL = "https://api.github.com/users/{user}/repos"
 
-def most_watched_repos(user, n):
-    url = URL.format(user=user)
-    resp = requests.get(url, timeout=10)
-    repos_list = json.loads(resp.content)
+def sort_response_json(text, n):
+    repos_list = json.loads(text)
     return [i['html_url'] for i in sorted(
         repos_list, key=lambda i: -1 * i['watchers'])][:n]
+
+
+def most_watched_repos(user, n):
+    url = URL.format(user=user)
+    resp = requests.get(url, timeout=0.1)
+    return sort_response_json(resp.content, n)
 
 
 if __name__ == '__main__':
--- test_your_code/examples/my_repos/test_myrepos.py
+++ test_your_code/examples/my_repos/test_myrepos.py
@@ -1,5 +1,6 @@
-from myrepos import most_watched_repos
+from myrepos import most_watched_repos, sort_response_json
 import unittest
+import mock
 
 
 class TestMyRepos(unittest.TestCase):
@@ -9,3 +10,20 @@ class TestMyRepos(unittest.TestCase):
         resp = most_watched_repos('rtnpro', 10)
         self.assertEqual(resp, expected_resp)
 
+
+class TestMockedMyRepos(unittest.TestCase):
+
+    @mock.patch('myrepos.requests.get')
+    def test_success(self, mock_requests_get):
+        m = mock.Mock()
+        m.content = '[{"watchers": 0, "html_url": "foo"}, {"watchers": 4, "html_url": "foobar"}, {"watchers": 1, "html_url": "blah"}]'
+        mock_requests_get.return_value = m
+        resp = most_watched_repos('blah', 10)
+        self.assertEqual(resp, ["foobar", "blah", "foo"])
+
+
+class TestRefactoredMyRepos(unittest.TestCase):
+
+    def test_success(self):
+        resp_json = '[{"watchers": 0, "html_url": "foo"}, {"watchers": 4, "html_url": "foobar"}, {"watchers": 1, "html_url": "blah"}]'
+        self.assertEqual(sort_response_json(resp_json, 10), ["foobar", "blah", "foo"])
